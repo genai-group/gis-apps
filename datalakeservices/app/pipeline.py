58f835b359295a0ddcdb3e86af316e387d74edfa
Matt Poulton <getgenai@gmail.com>
2023-12-29 14:14:55 -0700

    adding more details to the flight manifest use case

--- datalakeservices/app/pipeline.py
+++ datalakeservices/app/pipeline.py
@@ -8,8 +8,26 @@ from config.init import *
 data = manifest_data_0
 
 #%%
+# Getting ready to copy/paste data
+passenger_info = data['passenger_info']
+
+# Adding flight manifest and flight number to each persons data
+new_data = []
+for obj in passenger_info:
+    obj['flight_manifest_id'] = data['flight_information']['flight_manifest_id']
+    obj['flight_number'] = data['flight_information']['flight_number']
+    obj['time_to_departure'] = data['flight_information']['time_to_departure']
+    new_data.append(obj)
+
+#%%
 # Reading in the template file
 template_dir = './config/templates'
+
+##################################
+####    Flight Information    ####
+##################################
+
+
 
 #%%
 # Flight
@@ -19,7 +37,77 @@ parse_config = yaml.safe_load(open(f"{template_dir}/fake_airline_manifest_flight
 # Filter data file
 if 'primary_key' in parse_config['template']:
     data = manifest_data_0[parse_config['template']['primary_key']]
+    if not isinstance(data, list):
+        data = [data]       
 
+#%%
+# Rename Fields
+if 'rename_fields' in parse_config:
+    data = rename_properties(data, parse_config['rename_fields'])
+
+#%%
+# Drop fields
+if 'drop_fields' in parse_config:
+    drop_fields = parse_config['drop_fields']
+
+if len(drop_fields) > 0:
+    clean_objects = []
+    for obj in data:
+        clean_objects.append({k:v for k,v in obj.items() if k not in drop_fields})
+    data = clean_objects
+
+#%%
+# Hashify
+data = hashify(data, namespace='flight_manifest')
+if not isinstance(data, list):
+    data = [data]       
+
+#%%
+# Load data into MongoDB Collection
+mongo_collection.insert_many(data)
+
+#%%
+# Entities
+if 'entities' in parse_config.keys():
+    entities = parse_config['entities']
+
+if len(entities) > 0:
+    for entity in entities:
+        # Loading an array of entities into Neo4j
+        neo4j_objects = map_func(lambda x: {k:v for k,v in x.items() if k in ['_guid', entity]}, data)
+
+
+>>>>>>>>>   you are right here     <<<<<<<<<<<<<<<<
+
+neo4j_objects = standardize_objects(data, parse_config)
+print(neo4j_objects)
+
+
+
+
+
+
+
+#%%
+# Load Passengers into Neo4j
+neo4j_objects = map_func(lambda x: {k:v for k,v in x.items() if k in ['_guid','name']}, data)
+
+load_statement = ', '.join([f'`{k}`: line.`{k}`' for k in neo4j_objects[0].keys() if k not in ['_relationship']])
+load_statement = f'UNWIND $objects AS line MERGE (obj:Object {{ {load_statement} }})'
+
+#%%
+# Load objects given the schema
+with neo4j_client.session() as session:
+    session.run(load_statement, objects=neo4j_objects)
+    logging.info(f'Loaded Objects into Neo4j: {neo4j_objects}')
+    print(f"Loaded Objects into Neo4j: {len(neo4j_objects)}")
+
+
+
+#####################################
+####    Passenger Information    ####
+#####################################
+    
 #%%
 # Passengers
 parse_config = yaml.safe_load(open(f"{template_dir}/fake_airline_manifest_passengers.yml", "r").read())
