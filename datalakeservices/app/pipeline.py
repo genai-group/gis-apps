e0d08ad0f305330e7e3c5aff2358dca0c2d30c4d
Matt Poulton <getgenai@gmail.com>
2023-12-29 14:20:09 -0700

    adding more details to the flight manifest use case

--- datalakeservices/app/pipeline.py
+++ datalakeservices/app/pipeline.py
@@ -27,8 +27,6 @@ template_dir = './config/templates'
 ####    Flight Information    ####
 ##################################
 
-
-
 #%%
 # Flight
 parse_config = yaml.safe_load(open(f"{template_dir}/fake_airline_manifest_flight.yml", "r").read())
@@ -186,79 +184,3 @@ if 'entities' in parse_config.keys():
             print(f"entity: {entity}")
             'standardize_' in entity
 
-#%%
-def standardize_objects(objects List[Dict], parse_config: List[Dict]) -> List[Dict]:
-    """
-        Standardize objects based on the parse_config
-
-        Args:
-            objects (list): List of objects to standardize
-            parse_config (dict): The parse config file
-
-        Returns:
-            list: List of standardized objects
-
-    """
-    # Assertions
-
-    if 'standardize_fields' in parse_config.keys():            
-        for standardize_field in parse_config['standardize_fields']:
-            if standardize_field['transform'] in globals().keys():
-                print(map_func(lambda x: {'namespace':standardize_field['field'], 'label': x[standardize_field['field']], '_guid': hashify(globals()[(standardize_field['transform'])](x[standardize_field['field']]), namespace=standardize_field['field'])['_guid'], '_source':x['_guid'], '_relationship':'has_' + standardize_field['field']}, data))
-
-#%%
-
-
-#%%
-passengers = manifest_data_0['passenger_info']
-
-
-#%%
-
-# Building the parser:
-
-data = manifest_data_0['passenger_info']
-
-"""
-{
-    'full_name': 'Ryan Smith',
-    'gender': 'Male',
-    'date_of_birth': '1960-03-20',
-    'country_of_citizenship': 'Ireland',
-    'government_id_type': 'Passport',
-    'passport_information': '217-45-9511'
-}
-
-"""
-
-schema = """full_name [name], country_of_citizenship [country], passport_information [passport_id]; passport_id -> name, passport_id -> country"""
-
-# Split into statements
-statements = schema.split(';')
-
-statement = statements[0]
-
-for statement in statements:
-    commands = statement.split(',')
-    for command in commands:
-        print(f"command: {command.strip()}")
-        # search for the namespace
-        if '[' in command:
-            namespace = command.split('[')[1].split(']')[0]
-            variable = command.split('[')[0].strip()
-            print(f"namespace: {namespace}")
-            # print(f"variable: {variable}")
-        if '->' in command:
-            # This is a relation
-            parent = command.split('->')[0].strip()
-            # print(f"relation: {parent}")
-            # This is an attribute
-            children = command.split('->')[1].strip()
-            children = children.split(',')
-            print(f"children: {children}")
-            for child in children:
-                define_relation = 'has_' + child.strip().lower().replace(' ','_')
-                print(f"parent: {parent} => {define_relation} => {child}")
-                define_relation = None
-
-# %%
