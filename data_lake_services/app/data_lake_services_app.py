bb438bcdb5842872df2b6450caa15e50c7496ccb
Matt Poulton <getgenai@gmail.com>
2024-01-10 12:09:42 -0700

    going to local variable

--- data_lake_services/app/data_lake_services_app.py
+++ data_lake_services/app/data_lake_services_app.py
@@ -6,24 +6,26 @@ from config.config_init import *
 from flask import Flask, jsonify, request
 from flask_swagger_ui import get_swaggerui_blueprint
 
-#%%
 app = Flask(__name__)
 
 # Swagger UI configuration
-SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = '/static/swagger.json'  # Our API url (can be a local resource or remote)
+SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI
+API_URL = '/static/swagger.json'  # URL for the Swagger JSON
 
+# Create a Swagger UI blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
-    config={  # Swagger UI config overrides
+    config={
         'app_name': "GIS Data Lake Services"
     }
 )
 
+# Register the blueprint with Flask app
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 @app.route('/')
 def home():
-    # This route will display a simple message or HTML page
     return "<h1>Welcome to the Flask API!</h1>"
 
 if __name__ == '__main__':
