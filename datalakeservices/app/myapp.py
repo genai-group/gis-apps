471a0123cd3abaf5329020b2c659c917654ed6fc
Matt Poulton <getgenai@gmail.com>
2024-01-09 23:24:14 -0700

    adding flask app

--- datalakeservices/app/myapp.py
+++ datalakeservices/app/myapp.py
@@ -26,21 +26,5 @@ def home():
     # This route will display a simple message or HTML page
     return "<h1>Welcome to the Flask API!</h1>"
 
-@app.route('/api/item', methods=['POST'])
-def create_item():
-    data = request.json
-    return jsonify({"message": "Item created", "data": data}), 201
-
-@app.route('/api/item/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
-    data = request.json
-    return jsonify({"message": f"Item {item_id} updated", "data": data}), 200
-
-@app.route('/api/item/<int:item_id>', methods=['GET'])
-def get_item(item_id):
-    # Example: Fetch item details from your data source
-    item = {"id": item_id, "name": "Example Item", "description": "This is an example."}
-    return jsonify(item), 200
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
