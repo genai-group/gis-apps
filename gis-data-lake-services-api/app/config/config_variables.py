d9e6d4342d21ab1ced8c42a196950e8d73638b5b
Matt Poulton <getgenai@gmail.com>
2024-01-19 13:14:56 -0700

    removing volumes

--- gis-data-lake-services-api/app/config/config_variables.py
+++ gis-data-lake-services-api/app/config/config_variables.py
@@ -4,8 +4,8 @@
 import os
 
 #%%
-GIS_ENVIRONMENT = 'local'
-# GIS_ENVIRONMENT = 'flask-local'
+# GIS_ENVIRONMENT = 'local'
+GIS_ENVIRONMENT = 'flask-local'
 
 MINIO_ENDPOINT_URL = os.environ.get('MINIO_ENDPOINT_URL', 'localhost:9000')
 MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY', 'minio')
